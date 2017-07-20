c87ac1f3b536038cfbedb8f73cbdaddd3c74dda2
Jens Schulze <info@jensschulze3000.de>
2017-07-21 00:39:01 +0200

    Comment unused codes in factory

--- src/Jbarc/Barcode/Factory.php
+++ src/Jbarc/Barcode/Factory.php
@@ -61,118 +61,118 @@ class Factory
                 $generator = new Code39($process);
                 break;
             }
-            case 'C93': { // CODE 93 - USS-93
+//            case 'C93': { // CODE 93 - USS-93
 //                $arrcode = $this->barcode_code93($code);
-                break;
-            }
-            case 'S25': { // Standard 2 of 5
+//                break;
+//            }
+//            case 'S25': { // Standard 2 of 5
 //                $arrcode = $this->barcode_s25($code, false);
-                break;
-            }
-            case 'S25+': { // Standard 2 of 5 + CHECKSUM
+//                break;
+//            }
+//            case 'S25+': { // Standard 2 of 5 + CHECKSUM
 //                $arrcode = $this->barcode_s25($code, true);
-                break;
-            }
-            case 'I25': { // Interleaved 2 of 5
+//                break;
+//            }
+//            case 'I25': { // Interleaved 2 of 5
 //                $arrcode = $this->barcode_i25($code, false);
-                break;
-            }
-            case 'I25+': { // Interleaved 2 of 5 + CHECKSUM
+//                break;
+//            }
+//            case 'I25+': { // Interleaved 2 of 5 + CHECKSUM
 //                $arrcode = $this->barcode_i25($code, true);
-                break;
-            }
-            case 'C128': { // CODE 128
+//                break;
+//            }
+//            case 'C128': { // CODE 128
 //                $arrcode = $this->barcode_c128($code, '');
-                break;
-            }
-            case 'C128A': { // CODE 128 A
+//                break;
+//            }
+//            case 'C128A': { // CODE 128 A
 //                $arrcode = $this->barcode_c128($code, 'A');
-                break;
-            }
-            case 'C128B': { // CODE 128 B
+//                break;
+//            }
+//            case 'C128B': { // CODE 128 B
 //                $arrcode = $this->barcode_c128($code, 'B');
-                break;
-            }
-            case 'C128C': { // CODE 128 C
+//                break;
+//            }
+//            case 'C128C': { // CODE 128 C
 //                $arrcode = $this->barcode_c128($code, 'C');
-                break;
-            }
-            case 'EAN2': { // 2-Digits UPC-Based Extension
+//                break;
+//            }
+//            case 'EAN2': { // 2-Digits UPC-Based Extension
 //                $arrcode = $this->barcode_eanext($code, 2);
-                break;
-            }
-            case 'EAN5': { // 5-Digits UPC-Based Extension
+//                break;
+//            }
+//            case 'EAN5': { // 5-Digits UPC-Based Extension
 //                $arrcode = $this->barcode_eanext($code, 5);
-                break;
-            }
-            case 'EAN8': { // EAN 8
-                $pad     = (new ZeroPaddingProcess())->setPadLength(8);
-                $process = new ChainProcess([$pad,]);
-
-//                $arrcode = $this->barcode_eanupc($code, 8);
-                break;
-            }
+//                break;
+//            }
+//            case 'EAN8': { // EAN 8
+//                $pad     = (new ZeroPaddingProcess())->setPadLength(8);
+//                $process = new ChainProcess([$pad,]);
+//
+//          //      $arrcode = $this->barcode_eanupc($code, 8);
+//                break;
+//            }
             case 'EAN13': { // EAN 13
                 $process   = (new ZeroPaddingProcess())->setPadLength(13);
                 $generator = new Ean13($process);
                 break;
             }
-            case 'UPCA': { // UPC-A
+//            case 'UPCA': { // UPC-A
 //                $arrcode = $this->barcode_eanupc($code, 12);
-                break;
-            }
-            case 'UPCE': { // UPC-E
+//                break;
+//            }
+//            case 'UPCE': { // UPC-E
 //                $arrcode = $this->barcode_eanupc($code, 6);
-                break;
-            }
-            case 'MSI': { // MSI (Variation of Plessey code)
+//                break;
+//            }
+//            case 'MSI': { // MSI (Variation of Plessey code)
 //                $arrcode = $this->barcode_msi($code, false);
-                break;
-            }
-            case 'MSI+': { // MSI + CHECKSUM (modulo 11)
+//                break;
+//            }
+//            case 'MSI+': { // MSI + CHECKSUM (modulo 11)
 //                $arrcode = $this->barcode_msi($code, true);
-                break;
-            }
-            case 'POSTNET': { // POSTNET
+//                break;
+//            }
+//            case 'POSTNET': { // POSTNET
 //                $arrcode = $this->barcode_postnet($code, false);
-                break;
-            }
-            case 'PLANET': { // PLANET
+//                break;
+//            }
+//            case 'PLANET': { // PLANET
 //                $arrcode = $this->barcode_postnet($code, true);
-                break;
-            }
-            case 'RMS4CC': { // RMS4CC (Royal Mail 4-state Customer Code) - CBC (Customer Bar Code)
+//                break;
+//            }
+//            case 'RMS4CC': { // RMS4CC (Royal Mail 4-state Customer Code) - CBC (Customer Bar Code)
 //                $arrcode = $this->barcode_rms4cc($code, false);
-                break;
-            }
-            case 'KIX': { // KIX (Klant index - Customer index)
+//                break;
+//            }
+//            case 'KIX': { // KIX (Klant index - Customer index)
 //                $arrcode = $this->barcode_rms4cc($code, true);
-                break;
-            }
-            case 'IMB': { // IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200
+//                break;
+//            }
+//            case 'IMB': { // IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200
 //                $arrcode = $this->barcode_imb($code);
-                break;
-            }
-            case 'IMBPRE': { // IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200- pre-processed
+//                break;
+//            }
+//            case 'IMBPRE': { // IMB - Intelligent Mail Barcode - Onecode - USPS-B-3200- pre-processed
 //                $arrcode = $this->barcode_imb_pre($code);
-                break;
-            }
-            case 'CODABAR': { // CODABAR
+//                break;
+//            }
+//            case 'CODABAR': { // CODABAR
 //                $arrcode = $this->barcode_codabar($code);
-                break;
-            }
-            case 'CODE11': { // CODE 11
+//                break;
+//            }
+//            case 'CODE11': { // CODE 11
 //                $arrcode = $this->barcode_code11($code);
-                break;
-            }
-            case 'PHARMA': { // PHARMACODE
+//                break;
+//            }
+//            case 'PHARMA': { // PHARMACODE
 //                $arrcode = $this->barcode_pharmacode($code);
-                break;
-            }
-            case 'PHARMA2T': { // PHARMACODE TWO-TRACKS
+//                break;
+//            }
+//            case 'PHARMA2T': { // PHARMACODE TWO-TRACKS
 //                $arrcode = $this->barcode_pharmacode2t($code);
-                break;
-            }
+//                break;
+//            }
             default:
                 throw new InvalidArgumentException("Barcode type '$name' unknown");
                 break;
